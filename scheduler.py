8630188d7befb6196b93d3a01cc839f70e28258e
MegAMog <nastymarch.education@gmail.com>
2025-10-13 22:39:41 +0100

    Updated scheduler.py (minor changes).

--- scheduler.py
+++ scheduler.py
@@ -4,7 +4,10 @@ from script import run_stock_ticker_fetcher
 
 from datetime import datetime
 
+
 # Run every 10 minutes
+print(f"Scheduler started at {datetime.now()}")
+print("Fetching stock tickers every 10 minutes...")
 schedule.every(10).minutes.do(run_stock_ticker_fetcher)
 
 # # Run every day at 09:30 AM
