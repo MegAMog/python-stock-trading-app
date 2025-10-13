2861d87c449ff726cdf4d6c7b2e0f18b535265fb
MegAMog <nastymarch.education@gmail.com>
2025-10-13 21:58:28 +0100

    Added scheduler that will run script.py each 10 minutes.

---
+++ scheduler.py
@@ -0,0 +1,15 @@
+import schedule
+import time
+from script import run_stock_ticker_fetcher
+
+from datetime import datetime
+
+# Run every 10 minutes
+schedule.every(10).minutes.do(run_stock_ticker_fetcher)
+
+# # Run every day at 09:30 AM
+# schedule.every().day.at("09:30").do(run_stock_ticker_fetcher)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
