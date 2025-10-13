803c833de0bc336b86cff12ee4a8b7187360d6f0
MegAMog <nastymarch.education@gmail.com>
2025-10-13 21:26:46 +0100

    Updated script.py so it can fetch all data (using 1 min pause in case of exceded request limit.

--- script.py
+++ script.py
@@ -1,13 +1,14 @@
 import requests
 import os
 import csv
+import time
 
 from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
 # Function to fetch active stock tickers and save to CSV-file.
-def run_stock_ticker_fetcher(API_KEY=None, next_url=None):
+def run_stock_ticker_fetcher(API_KEY=None):
     # Request parameters
     if API_KEY is None:
         API_KEY = os.getenv("POLYGON_API_KEY")  # Get API key from environment variable if not provided
@@ -15,15 +16,12 @@ def run_stock_ticker_fetcher(API_KEY=None, next_url=None):
     LIMIT=1000 # Number of tickers to fetch per request
 
     # API endpoint for fetching active stock tickers
-    if next_url is None:
-        url=f"https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={API_KEY}"
-    else:
-        url=next_url+f'&apiKey={API_KEY}'
+    url=f"https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={API_KEY}"
 
     # Fetch the first page of ticker data
     response = requests.get(url)
     data = response.json()
-    page_number=LIMIT
+    page_number= data.get('count',0)
 
     # Initialize list to store all tickers
     tickers = []
@@ -34,18 +32,23 @@ def run_stock_ticker_fetcher(API_KEY=None, next_url=None):
     error_pricing="You've exceeded the maximum requests per minute, please wait or upgrade your subscription to continue. https://polygon.io/pricing"
 
     # Fetch additional pages if 'next_url' is present
-    while 'next_url' in data:
-        response = requests.get(data['next_url']+f'&apiKey={API_KEY}')
+    next_url = data.get('next_url','end')
+    while next_url and next_url != 'end':
+        response = requests.get(next_url+f'&apiKey={API_KEY}')
         data = response.json()
-
+       
         if data.get("error")==error_pricing:
-            break
+            print(f"Fetched {page_number} tickers so far.")
+            print("Rate limit exceeded. Waiting 1 minute...")
+            time.sleep(60)
+            continue
         else:
             next_url = data.get('next_url','end')
+
             for ticker in data['results']:
                 tickers.append(ticker)
-        
-        page_number+=LIMIT
+            
+            page_number+=data.get('count',0)
 
     # Define output CSV file path
     output_csv='data/tickers.csv'
@@ -57,7 +60,7 @@ def run_stock_ticker_fetcher(API_KEY=None, next_url=None):
     write_header = not os.path.exists(output_csv) or os.path.getsize(output_csv) == 0
 
     # Write all ticker data into a CSV file
-    with open(output_csv, mode='a', newline='') as file:
+    with open(output_csv, mode='w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader() # Write header row
         for ticker in tickers:
@@ -65,11 +68,9 @@ def run_stock_ticker_fetcher(API_KEY=None, next_url=None):
             row = {key: ticker.get(key,'') for key in fieldnames}
             writer.writerow(row)
     
-
-    return next_url, page_number
+    return page_number
 
 if __name__ == "__main__":
-    next_url, page_number = run_stock_ticker_fetcher(next_url='https://api.polygon.io/v3/reference/tickers?cursor=YWN0aXZlPXRydWUmYXA9MTAwMDAmYXM9JmxpbWl0PTEwMDAmbWFya2V0PXN0b2NrcyZvcmRlcj1hc2Mmc29ydD10aWNrZXI')
-    print(next_url)
+    page_number = run_stock_ticker_fetcher()
     print(f"Fetched {page_number} tickers and saved to data/tickers.csv")
     
